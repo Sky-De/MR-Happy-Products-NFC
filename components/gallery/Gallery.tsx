0da97667967a7097bab3c7cedbc9823dc289bc5a
SkyDe <Sky_de1991@outlook.com>
2023-10-20 09:29:47 -0700

    UI debug

--- components/gallery/Gallery.tsx
+++ components/gallery/Gallery.tsx
@@ -20,8 +20,8 @@ const Gallery = ({ products, isLoading }: GalleryType) => {
     return <h2>There is no product yet</h2>;
 
   return (
-    <section className=" h-full max-h-[85vh] md:max-h-screen z-0 pt-2 flex justify-center overflow-hidden max-w-[400px]">
-      <Carousel>
+    <section className=" h-full max-h-[85vh] md:max-h-screen z-0 w-screen pt-2 flex justify-center overflow-hidden max-w-[400px] mx-auto">
+      <Carousel className="">
         {products &&
           products?.map((product) => (
             <div key={product._id}>
